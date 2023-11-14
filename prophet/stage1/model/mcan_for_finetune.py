72304e3451227acbed90a806c24b8797a2a23ee3
Duong Tran <d@dttt.io>
2023-11-14 02:25:48 +0100

    ...

--- prophet/stage1/model/mcan_for_finetune.py
+++ prophet/stage1/model/mcan_for_finetune.py
@@ -20,6 +20,7 @@ class MCANForFinetune(MCAN):
     def __init__(self, __C, answer_size, base_answer_size=3129):
         super().__init__(__C, base_answer_size)
 
+        print(answer_size, base_answer_size)
         self.proj1 = nn.Linear(__C.FLAT_OUT_SIZE, answer_size - base_answer_size)
 
     @torch.no_grad()
