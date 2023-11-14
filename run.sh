42af0fbe1f17ff7f260af31e1890138ff4d27735
Duong Tran <d@dttt.io>
2023-11-14 17:29:40 +0100

    ...

--- run.sh
+++ run.sh
@@ -12,5 +12,12 @@ esac
 # DATA PREPARATION - Step 2
 # bash scripts/extract_img_feats.sh --dataset $DS_NAME
 
-# STAGE ONE - Step 4
-bash scripts/finetune.sh --task $DS_NAME --version ${DS_NAME}_finetune_1 --pretrained_model ckpts/mcan_pt_okvqa.pkl
+# STAGE ONE
+# Step 4
+# bash scripts/finetune.sh --task $DS_NAME --version ${DS_NAME}_finetune_1 --pretrained_model ckpts/mcan_pt_okvqa.pkl
+
+# Step 5
+bash scripts/heuristics_gen.sh \
+    --version ${DS_NAME}_heuristics_1
+    --ckpt_path outputs/${DS_NAME}_finetune_1/ckpts/epoch_6.pkl
+    --candidate_num 10 --example_num 100
