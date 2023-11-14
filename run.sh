d36d84fc9e6f00f044bab70cd01cd0456942ebfa
Duong Tran <d@dttt.io>
2023-11-14 01:12:53 +0100

    ...

--- run.sh
+++ run.sh
@@ -15,4 +15,10 @@ python test_gpu.py
 # bash scripts/extract_img_feats.sh --dataset $DS_NAME
 
 # STAGE ONE - Step 4
-bash scripts/finetune.sh --version ${DS_NAME}_finetune_1 --pretrained_model ckpts/mcan_pt_okvqa.pkl
+#bash scripts/finetune.sh --version ${DS_NAME}_finetune_1 --pretrained_model ckpts/mcan_pt_okvqa.pkl
+python main.py \
+    --task ok --run_mode finetune \
+    --cfg configs/finetune.yml \
+    --version ${DS_NAME}_finetune_1 \
+    --pretrained_model ckpts/mcan_pt_okvqa.pkl \
+    --gpu 0 --seed 99 --grad_accu 2
