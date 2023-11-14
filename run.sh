07426071bacd8686695e3e5021c39b8901f678e0
Duong Tran <d@dttt.io>
2023-11-14 20:07:28 +0100

    ...

--- run.sh
+++ run.sh
@@ -16,26 +16,26 @@ esac
 
 # STAGE ONE
 # Step 3
-bash scripts/pretrain.sh --task $DS_NAME --version ${DS_NAME}_pretrain_${$EXP}
+bash scripts/pretrain.sh --task $DS_NAME --version ${DS_NAME}_pretrain_${N}
 
 # Step 4
-# bash scripts/finetune.sh --task $DS_NAME --version ${DS_NAME}_finetune_${$EXP} --pretrained_model ckpts/mcan_pt_okvqa.pkl
+# bash scripts/finetune.sh --task $DS_NAME --version ${DS_NAME}_finetune_${N} --pretrained_model ckpts/mcan_pt_okvqa.pkl
 bash scripts/finetune.sh \
  --task $DS_NAME \
- --version ${DS_NAME}_finetune_${$EXP} \
- --pretrained_model outputs/ckpts/${DS_NAME}_pretrain_${$EXP}/epoch13.pkl
+ --version ${DS_NAME}_finetune_${N} \
+ --pretrained_model outputs/ckpts/${DS_NAME}_pretrain_${N}/epoch13.pkl
 
 # Step 5
 bash scripts/heuristics_gen.sh \
     --task $DS_NAME \
-    --version ${DS_NAME}_heuristics_${$EXP} \
-    --ckpt_path outputs/ckpts/${DS_NAME}_finetune_${$EXP}/epoch6.pkl \
+    --version ${DS_NAME}_heuristics_${N} \
+    --ckpt_path outputs/ckpts/${DS_NAME}_finetune_${N}/epoch6.pkl \
     --candidate_num 10 --example_num 100
 
 # STAGE TWO
 # Step 6
 #bash scripts/prompt.sh \
 #    --task $DS_NAME \
-#    --version ${DS_NAME}_prompt_${$EXP} \
-#    --examples_path outputs/results/${DS_NAME}_heuristics_${$EXP}/examples.json \
-#    --candidates_path outputs/results/${DS_NAME}_heuristics_${$EXP}/candidates.json \
+#    --version ${DS_NAME}_prompt_${N} \
+#    --examples_path outputs/results/${DS_NAME}_heuristics_${N}/examples.json \
+#    --candidates_path outputs/results/${DS_NAME}_heuristics_${N}/candidates.json \
