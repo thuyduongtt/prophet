7a05f2b0aa2b4748c636700b488cbc94c2ca34e8
Duong Tran <d@dttt.io>
2023-11-14 17:09:51 +0100

    ...

--- lsf_v100.sh
+++ lsf_v100.sh
@@ -1,4 +1,4 @@
-#BSUB -J PP[1-2]                 # Specify the job name
+#BSUB -J PP[1]                 # Specify the job name
 #BSUB -W 48:00                # Specify the maximum runtime in "hours:minutes"
 #BSUB -o %x.%j.out            # Determine where the output will be written
 #BSUB -e %x.%j.err            # The same goes for the error file
