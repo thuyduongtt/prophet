50c2bc7dd6f83773b06f7fc1d031dc17aaaec06d
Duong Tran <d@dttt.io>
2023-11-23 23:39:12 +0100

    + Switch to LLama instead of GPT

--- prophet/stage2/prompt.py
+++ prophet/stage2/prompt.py
@@ -101,7 +101,7 @@ class Runner:
 
         # response_txt = response.choices[0].text.strip()
         response_txt = response['choices'][0]['text'].strip()
-        print(response_txt)
+        # print(response_txt)
         plist = []
         for ii in range(len(response['choices'][0]['logprobs']['tokens'])):
             if response['choices'][0]['logprobs']['tokens'][ii] in ["\n", "<|endoftext|>"]:
